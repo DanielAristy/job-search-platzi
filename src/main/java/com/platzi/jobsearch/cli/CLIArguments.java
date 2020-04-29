b94ea55c69d5afa468d2c1e76366fb99f7c61662
DanielAristy <danielariaty@gmail.com>
2020-04-29 11:37:13 -0500

    Se crea la clase CLIArguments en la cual se van a tener todos los argumentos de ayuda para ser un poco mas amigable con la terminal

---
+++ src/main/java/com/platzi/jobsearch/cli/CLIArguments.java
@@ -0,0 +1,114 @@
+package com.platzi.jobsearch.cli;
+
+import com.beust.jcommander.Parameter;
+
+public class CLIArguments {
+
+    CLIArguments(){
+
+    }
+
+    @Parameter(
+            required = true,
+            descriptionKey = "KEYWORD",
+            description = "KEYWORD"
+    )
+
+    private String keyWord;
+
+    @Parameter(
+            names = {"--location", "-l"},
+            description = "Cada busqueda puede incluir una ubicacion"
+
+    )
+    private String location;
+
+    @Parameter(
+            names = {"--page","-p"},
+            description = "La API devuelve 50 resultados, usa un numero para la pagina"
+    )
+    private int page = 0;
+
+    @Parameter(
+            names = {"--full-time"},
+            description = "Agregar si queremos trabajos de tiempo completo"
+    )
+    private boolean isFullTime = false;
+
+    @Parameter(
+            names = {"--markdown"},
+            description = "Obtener los resultado en markdown"
+    )
+    private boolean isMarkDown = false;
+
+    @Parameter(
+            names = {"--help"},
+            help = true,
+            description = "Mostrar esta ayuda"
+    )
+    private boolean isHelp;
+
+    public String getKeyWord() {
+        return keyWord;
+    }
+
+    public void setKeyWord(String keyWord) {
+        this.keyWord = keyWord;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public boolean isFullTime() {
+        return isFullTime;
+    }
+
+    public void setFullTime(boolean fullTime) {
+        isFullTime = fullTime;
+    }
+
+    public boolean isMarkDown() {
+        return isMarkDown;
+    }
+
+    public void setMarkDown(boolean markDown) {
+        isMarkDown = markDown;
+    }
+
+    public boolean isHelp() {
+        return isHelp;
+    }
+
+    public void setHelp(boolean help) {
+        isHelp = help;
+    }
+
+    @Override
+    public String toString() {
+        return "CLIArguments{" +
+                "keyWord='" + keyWord + '\'' +
+                ", location='" + location + '\'' +
+                ", page=" + page +
+                ", isFullTime=" + isFullTime +
+                ", isMarkDown=" + isMarkDown +
+                ", isHelp=" + isHelp +
+                '}';
+    }
+
+    public static CLIArguments newInstance(){
+        return new CLIArguments();
+    }
+}
